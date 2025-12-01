4070ee187a9771ffecae5803c9cb713ca32d0307
OlhaRyzha <104225380+OlhaRyzha@users.noreply.github.com>
2025-12-01 15:59:42 +0200

    Update DATABASES configuration to use environment variables for production and SQLite for development

--- config/settings.py
+++ config/settings.py
@@ -61,12 +61,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT", "5432"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "NAME": os.getenv("DB_NAME"),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
